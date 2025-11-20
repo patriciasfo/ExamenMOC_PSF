1d47c484998419f718c7e7db9bd59fced1f173da
patriciasfo <patriciasfo@educastur.org>
2025-11-20 11:37:06 +0100

    Initial commit

---
+++ src/main/java/com/example/ExamenMOC_PSF/ExamenMocPsfApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMOC_PSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocPsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocPsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExamenMOC_PSF/ExamenMocPsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMOC_PSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocPsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
